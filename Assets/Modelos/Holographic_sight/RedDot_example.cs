e812ac96c2f9a0850b273246e25a493711843865
ctYurk15 <levgenetic@gmail.com>
2023-08-16 21:11:58 +0300

    Configure initial state for M16A4

---
+++ Assets/Modelos/Holographic_sight/RedDot_example.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedDot_example : MonoBehaviour {
+
+	public GameObject redDot;
+	public bool isAiming = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Mouse1)) {
+			isAiming = true;
+
+		} else {
+			isAiming = false;
+		}
+	
+{
+
+		  if (isAiming == true) {
+				redDot.GetComponent<Renderer>().enabled = true;
+			}
+			else 
+			{
+				redDot.GetComponent<Renderer>().enabled = false;
+			}
+		
+
+
+		}
+
+
+	
+	}
+}
+
+
+
+
